080874f6ca7b459a0df8bc0462f5c7b562909c6c
RahulRj09 <rahul18@navgurukul.org>
2019-03-08 07:29:08 +0530

    completed code

--- saral.js
+++ saral.js
@@ -7,7 +7,7 @@ app.get('/courses', (req, res, next) =>{
         console.log( data );
         res.end( data );
      });
-})                
+})              
 
 app.get('/courses/:id', (req, res, next) =>{
     fs.readFile( __dirname + "/" + "courses.json", 'utf8', (err, data) => {
@@ -84,22 +84,169 @@ app.put('/courses/:id', (req, res, next) =>{
 })
 
 app.get('/courses/:id/exercises', (req,res,next) =>{
-    fs.readFile( __dirname + "/" + "excercise.json", 'utf8', (err, data) => {
+    fs.readFile( __dirname + "/" + "exercise.json", 'utf8', (err, data) => {
         console.log("aagya hai")
         let data1 = []
         let coures_exercises = JSON.parse( data );
+        console.log(data)
         for(let i = 0; i < coures_exercises.length; i++){
-            console.log("ssss")
             if(req.params.id == coures_exercises[i]['courseId']){
-            console.log(coures_exercises)
+            console.log(coures_exercises[i])
             data1.push(coures_exercises[i])
             }
         }
+        console.log(data1)
         res.send(data1)
     });
 
 });
 
+app.post('/courses/:id/exercises', (req, res, next) =>{
+    fs.access(__dirname + "/" + "exercise.json", fs.F_OK, (err) => {
+        if (err) {
+          console.error(err)
+        
+        }else{
+            console.log("hai file");
+            fs.readFile( __dirname + "/" + "exercise.json", 'utf8', (err, data) => {
+                if (err) {
+                  console.error("open error:  " + err);
+                } else if(req.body.name && req.body.content && req.body.hint && Object.keys(req.body).length == 3) {
+                    let coures_data = JSON.parse(data);
+                    console.log("json hai")
+                    console.log("Successfully opened",coures_data);
+                    console.log(req.params.id)
+                    const coures_name ={
+                        "id": coures_data.length+1,
+                        "courseId": parseInt(req.params.id),
+                        "name": req.body.name,
+                        "content": req.body.content,
+                        "hint": req.body.hint
+                     }
+                  coures_data.push(coures_name)
+                //   console.log(coures_data)
+                  fs.writeFile( __dirname + "/"+"exercise.json", JSON.stringify(coures_data,null, 2),   (err) => {
+                    if (err) 
+                        return console.log(err);
+                    });
+                    res.end( JSON.stringify(coures_name));
+                }else{
+                    res.send("aapki key sahi nahi hai")
+                }
+              });
+        }
+      
+    });
+})
 
 
-app.listen(5005);
+app.get('/courses/:cid/exercises/:eid', (req, res, next) =>{
+    fs.readFile( __dirname + "/" + "exercise.json", 'utf8', (err, data) => {
+        if(!err){
+            let coures_exercise = JSON.parse( data );
+            console.log(data)
+            for(let i = 0; i < coures_exercise.length; i++){
+                if (coures_exercise[i]['courseId'] === parseInt(req.params.cid)){
+                    if(coures_exercise[i]['id'] === parseInt(req.params.eid)){
+                        console.log("aggya hai json m");
+                        console.log(coures_exercise[i])
+                        return res.json(coures_exercise[i]);
+                    }else if (i == coures_exercise.length -1){
+                        res.json({errorMsg:"some error"})
+                    
+                    }    
+                }else if (i == coures_exercise.length-1){
+                    res.json({errorMsg:"some error"})
+                
+                }
+            }
+        } else{
+            console.log(err)
+        }
+     });
+})
+
+app.put('/courses/:cid/exercises/:eid', (req, res, next) =>{
+    fs.readFile( __dirname + "/" + "exercise.json", 'utf8', (err, data) => {
+        let coures_exercise = JSON.parse( data );
+
+            if (req.params.eid -1 < coures_exercise.length && req.params.eid >=0){
+                if(req.body.hasOwnProperty('name')){
+                    coures_exercise[req.params.eid-1]['name'] = req.body.name;
+                }
+                if(req.body.hasOwnProperty('content')){
+                    coures_exercise[req.params.eid-1]['content'] = req.body.content;
+                }
+                if(req.body.hasOwnProperty('hint')){
+                    coures_exercise[req.params.eid-1]['hint'] = req.body.hint;
+                }
+                fs.writeFile( __dirname + "/"+"exercise.json", JSON.stringify(coures_exercise,null, 2),(err) => {
+                    if (err){return console.log(err);} 
+
+                res.send(JSON.stringify(coures_exercise[req.body.eid-1]));
+                });
+            }else{
+                res.status(404).send("data not found");
+            }
+        
+     });
+})
+
+app.get('/courses/:cid/exercises/:eid/submissions', (req, res, next) =>{
+    fs.readFile( __dirname + "/" + "submissions.json", 'utf8', (err, data) => {
+        let coures_exercise = JSON.parse( data );
+        const submissions = []
+        if(!err){
+            for(let i = 0; i <coures_exercise.length; i++){
+                if(coures_exercise[i]['exerciseId'] === parseInt(req.params.eid) && coures_exercise[i]['courseId'] === parseInt(req.params.cid) ){
+                    submissions.push(coures_exercise[i])
+                }else if(i === coures_exercise.length-1 && submissions.length === 0){
+                    res.send("data nahi hai")
+                }
+            }if(submissions.length >0){
+                return res.json(submissions)
+            }
+            
+        }else{
+            return res.send("file nahi hai");
+        }
+        
+    })
+})
+
+app.post('/courses/:cid/exercises/:eid/submissions', (req, res, next) =>{
+    fs.access(__dirname + "/" + "submissions.json", fs.F_OK, (err) => {
+        if (err) {
+          console.error(err)
+        
+        }else{
+            console.log("hai file");
+            fs.readFile( __dirname + "/" + "submissions.json", 'utf8', (err, data) => {
+                if (err) {
+                  console.error("open error:  " + err);
+                } else if(req.body.content && req.body.userName && Object.keys(req.body).length == 2) {
+                    let coures_data = JSON.parse(data);
+                    const coures_name ={
+                        "id": coures_data.length+1,
+                        "courseId": parseInt(req.params.cid),
+                        "exerciseId": parseInt(req.params.eid),
+                        "content": req.body.content,
+                        "userName": req.body.userName
+                     }
+                  coures_data.push(coures_name)
+                //   console.log(coures_data)
+                  fs.writeFile( __dirname + "/"+"submissions.json", JSON.stringify(coures_data,null, 2),   (err) => {
+                    if (err) 
+                        return console.log(err);
+                    });
+                    res.end( JSON.stringify(coures_name));
+                }else{
+                    res.send("aapki key sahi nahi hai")
+                }
+              });
+        }
+      
+    });
+})
+
+app.listen(7001);
